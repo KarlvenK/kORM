7279b5fab0a8c0004292f5cf163d739205426414
Karlven <karlven@outlook.com>
2021-07-17 22:53:21 +0800

    schema

---
+++ schema/schema.go
@@ -0,0 +1,55 @@
+package schema
+
+import (
+	"github.com/KarlvenK/korm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+//Field represents a column of database
+type Field struct {
+	Name 	string
+	Type 	string
+	Tag 	string //约束条件，例如非空、主键等
+}
+
+//Schema represents a table of database
+type Schema struct {
+	Model 		interface{}
+	Name 		string
+	Fields 		[]*Field
+	FieldNames 	[]string
+	fieldMap 	map[string]*Field
+}
+
+//GetField returns field by name
+func (schema *Schema) GetField(name string) *Field {
+	return schema.fieldMap[name]
+}
+
+//Parse a struct to a schema instance
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	schema := &Schema{
+		Model: dest,
+		Name: modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modelType.NumField(); i++ {
+		p := modelType.Field(i)
+		if !p.Anonymous && ast.IsExported(p.Name) {
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v, ok := p.Tag.Lookup("korm"); ok {
+				field.Tag = v
+			}
+			schema.Fields = append(schema.Fields, field)
+			schema.FieldNames = append(schema.FieldNames, p.Name)
+			schema.fieldMap[p.Name] = field
+		}
+	}
+	return schema
+}
