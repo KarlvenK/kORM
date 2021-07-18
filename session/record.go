0f508b228ede20f48458e94c27071089d8c2679d
Karlven <karlven@outlook.com>
2021-07-18 17:16:45 +0800

    UPDATE & DELETE &COUNT

--- session/record.go
+++ session/record.go
@@ -24,6 +24,20 @@ func (s *Session) Insert(values ...interface{}) (int64, error) {
 }
 
 //Find gets all eligible records
+/*
+destSlice.Type().Elem() 获取切片的单个元素的类型 destType，
+使用 reflect.New() 方法创建一个 destType 的实例，作为 Model() 的入参，
+映射出表结构 RefTable()。
+
+根据表结构，使用 clause 构造出 SELECT 语句，查询到所有符合条件的记录 rows。
+
+遍历每一行记录，利用反射创建 destType 的实例 dest，将 dest 的
+所有字段平铺开，构造切片 values。
+
+调用 rows.Scan() 将该行记录每一列的值依次赋值给 values 中的每一个字段。
+
+将 dest 添加到切片 destSlice 中。循环直到所有的记录都添加到切片 destSlice 中.
+*/
 func (s *Session) Find(values interface{}) error {
 	destSlice := reflect.Indirect(reflect.ValueOf(values))
 	destType := destSlice.Type().Elem()
@@ -50,17 +64,47 @@ func (s *Session) Find(values interface{}) error {
 	return rows.Close()
 }
 
-/*
-destSlice.Type().Elem() 获取切片的单个元素的类型 destType，
-使用 reflect.New() 方法创建一个 destType 的实例，作为 Model() 的入参，
-映射出表结构 RefTable()。
+//support map[string]interface{}
+//also support kv list: 'Name", "Tom", "Age". 18,...
 
-根据表结构，使用 clause 构造出 SELECT 语句，查询到所有符合条件的记录 rows。
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	m, ok := kv[0].(map[string]interface{})
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
+	}
 
-遍历每一行记录，利用反射创建 destType 的实例 dest，将 dest 的
-所有字段平铺开，构造切片 values。
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
 
-调用 rows.Scan() 将该行记录每一列的值依次赋值给 values 中的每一个字段。
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
 
-将 dest 添加到切片 destSlice 中。循环直到所有的记录都添加到切片 destSlice 中.
-*/
+//Delete records with where clause
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+//Count records with where clause
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
+}
